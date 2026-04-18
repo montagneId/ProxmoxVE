199174ab2beb0b762c135e974bbfb6023829ba30
Joost van den Berg <joost@montagnes.nl>
2026-04-18 13:25:27 +0200

    add path

--- install/umbraco-install.sh
+++ install/umbraco-install.sh
@@ -42,6 +42,8 @@ msg_info "Installing Umbraco templates and project (Patience)"
 cd /var/www/html
 $STD dotnet new install Umbraco.Templates@17.3.4 --force
 $STD dotnet new umbraco --force -n "$var_project_name"
+
+cd /var/www/html/$var_project_name
 $STD dotnet add package Our.Umbraco.PostgreSql
 msg_ok "Project Created"
 
